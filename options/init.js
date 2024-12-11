3079efd18ac466a5ad65211463e13ebf75e5c70f
naokin0525 <141907870+naokin0525@users.noreply.github.com>
2024-12-11 22:30:19 +0900

    Create init.js

---
+++ options/init.js
@@ -0,0 +1,19 @@
+/*
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+*/
+
+// Configs.jsのインスタンスを作成
+var configs = new Configs({
+  enterKeyTaaaaaaanMinSize: 80,
+  enterKeyTaaaaaaanMaxSize: 100,
+  normalKeyKataKataMinSize: 10,
+  normalKeyKataKataMaxSize: 20,
+  animateVerticalMove: 40,
+  animateHorizontalMove: 40,
+  debug: false,
+});
+
+// Optionsインスタンスを初期化
+var options = new Options(configs);
